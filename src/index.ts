acafd83f876c9c02aca663e5bc32eb94c2c476d5
Lucas Pazzim <lpazzim@gmail.com>
2021-11-30 15:14:37 -0300

    Sliding Window Pattern

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import { counterString, same, validAnagram, validAnagramCourseExample } from './FrequencyCounterPattern'
 import { nbDig } from './Codewars'
 import { countUniqueValues, sumZero, uniqueValues } from './MultiplePointersPattern';
+import { maxSubarraySum } from './SlidingWindowPattern';
 
 
 console.log('------------- FREQUENCY COUNTER PATTERN -------------')
@@ -26,6 +27,16 @@ console.log(countUniqueValues([-2,-1,0, 1]));
 
 console.log('------------- END MULTIPLE POINTERS PATTERN -------------')
 
+
+
+
+
+console.log('------------- SLINDING WINDOW PATTERN -------------')
+
+console.log(maxSubarraySum([2,6,9,2,1,8,5,6,27],3));
+
+
+console.log('------------- END SLINDING WINDOW PATTERN -------------')
 
 
 // console.log('------------- CODEWARS -------------')
