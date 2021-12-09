bfe5f6f563026804781638e7669afc515e3ed6c6
Lucas Pazzim <lpazzim@gmail.com>
2021-12-09 17:31:13 -0300

    Bubble Sort Optimized

--- src/SortingAlgorithms/BubbleSort/index.ts
+++ src/SortingAlgorithms/BubbleSort/index.ts
@@ -21,10 +21,53 @@ function bubbleSortES2015Version(arr: any) {
 
   for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
     }
+  }
+  return arr;
+}
+
+
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT *** Optimized with noSwaps ***
+function bubbleSortNoSwaps(arr: any) {
+  let noSwaps: boolean;
+  for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+// ES2015 Version *** Optimized with noSwaps ***
+function bubbleSortES2015VersionNoSwaps(arr: any) {
+  let noSwaps: boolean;
+  const swap = (arr: any, idx1: any, idx2: any) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    noSwaps = false;
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        console.log(arr, arr[j], arr[j + 1]);
+        swap(arr, j, j + 1);
+      }
+    }
+    if(noSwaps) break;
   }
   return arr;
 }
