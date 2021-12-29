7d8f977368ff21c7650bcd9592f73e1e9e6799ff
Lucas Pazzim <lpazzim@gmail.com>
2021-12-29 15:53:19 -0300

    DS Singly Linked Lists

---
+++ src/DataStructures/SinglyLinkedLists/index.ts
@@ -0,0 +1,23 @@
+class NodeClass {
+  val: any
+  next: any
+  constructor(val: any) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList{
+  head: any
+  tail: any
+  length: any
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(){
+
+  }
+}
