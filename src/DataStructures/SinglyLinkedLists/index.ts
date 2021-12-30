00af15a1e9828462b2e8450077ee4b97ea0a14d4
Lucas Pazzim <lpazzim@gmail.com>
2021-12-30 16:26:12 -0300

    Singly Linked List Unshift

--- src/DataStructures/SinglyLinkedLists/index.ts
+++ src/DataStructures/SinglyLinkedLists/index.ts
@@ -37,33 +37,45 @@ export class SinglyLinkedList {
   //    }
   //  }
 
-   pop(){
-    if(!this.head) return undefined;
+  pop() {
+    if (!this.head) return undefined;
     let current = this.head;
     let newTail = current;
-    while(current.next){
+    while (current.next) {
       newTail = current;
       current = current.next;
     }
     this.tail = newTail;
     this.tail.next = null;
     this.length--;
-    if(this.length === 0 ) {      
+    if (this.length === 0) {
       this.tail = null;
     }
     return current;
   }
 
-  shift(){
-    if(!this.head) return undefined
+  shift() {
+    if (!this.head) return undefined
     let currentHead = this.head;
     this.head = currentHead.next;
     this.length--;
-    if(this.length === 0 ) {
+    if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
     return currentHead;
+  }
+  unshift(val: any) {
+    var newNode = new NodeClass(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
   }
 
 }
