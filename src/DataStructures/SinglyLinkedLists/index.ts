f58c906038dfad68b8f9ce4f6c0d09425910a282
Lucas Pazzim <lpazzim@gmail.com>
2022-01-04 14:46:54 -0300

    Singly Linked List Remove

--- src/DataStructures/SinglyLinkedLists/index.ts
+++ src/DataStructures/SinglyLinkedLists/index.ts
@@ -111,4 +111,15 @@ export class SinglyLinkedList {
     return true;
   }
 
+  remove(index: number) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
 }
