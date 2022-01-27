12b82b2f674fa249560e81c550be034d914dc0ed
Lucas Pazzim <lpazzim@gmail.com>
2022-01-27 09:05:49 -0300

    Doubly Linked List push

--- src/DataStructures/DoublyLinkedLists/index.ts
+++ src/DataStructures/DoublyLinkedLists/index.ts
@@ -18,4 +18,18 @@ export class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val: any) {
+    var newNode = new NodeClass(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
