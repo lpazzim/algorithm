8940c154f3c3f14ae43c4d1987862302a1c8d62e
Lucas Pazzim <lpazzim@gmail.com>
2022-03-04 20:39:58 -0300

    BinarySearchTree

---
+++ src/DataStructures/BinarySearchTree/index.ts
@@ -0,0 +1,17 @@
+class NodeClass {
+  value: any
+  left: any
+  right: any
+  constructor(val: any) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  root: any
+  constructor() {
+    this.root = null;
+  }
+}
