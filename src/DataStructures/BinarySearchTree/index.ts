334bfce80e632345fb169cc4c485198abf3510e8
Lucas Pazzim <lpazzim@gmail.com>
2022-03-07 18:28:34 -0300

    BInary Search Tree insert

--- src/DataStructures/BinarySearchTree/index.ts
+++ src/DataStructures/BinarySearchTree/index.ts
@@ -14,4 +14,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val: any) {
+    let newNode = new NodeClass(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if(val === current.value) return undefined;
+        if (val < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
 }
