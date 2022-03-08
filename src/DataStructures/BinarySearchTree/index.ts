a9de5b41f6d1866d6fb010937c4295e9ebf4b3c0
Lucas Pazzim <lpazzim@gmail.com>
2022-03-08 18:58:05 -0300

    Binary Search Tree find

--- src/DataStructures/BinarySearchTree/index.ts
+++ src/DataStructures/BinarySearchTree/index.ts
@@ -23,7 +23,7 @@ class BinarySearchTree {
     } else {
       let current = this.root;
       while (true) {
-        if(val === current.value) return undefined;
+        if (val === current.value) return undefined;
         if (val < current.value) {
           if (current.left === null) {
             current.left = newNode;
@@ -41,6 +41,24 @@ class BinarySearchTree {
         }
       }
     }
+  }
+
+  find(val: any) {
+    if (this.root === null) return false;
+    let current: NodeClass = this.root;
+    let found: boolean = false;
+
+    while(current && !found){
+      if(val < current.value){
+        current = current.left;
+      } else if(val > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if(!found) return undefined;
+    return current;
   }
 
 }
