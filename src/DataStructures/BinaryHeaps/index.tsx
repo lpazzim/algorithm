fb0099cc3021838529acda74539da7257749ad9b
Lucas Pazzim <lpazzim@gmail.com>
2022-03-17 19:51:28 -0300

    BinaryHeaps insert

--- src/DataStructures/BinaryHeaps/index.tsx
+++ src/DataStructures/BinaryHeaps/index.tsx
@@ -3,4 +3,23 @@ export class BinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  insert(element: any) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      //Formula to find the parent element index "Math.floor((idx - 1) / 2)";
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
 }
