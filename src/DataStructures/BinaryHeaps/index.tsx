7bbfc974bac4a7617f0b99bf5b5ac6acc59eb5a0
Lucas Pazzim <lpazzim@gmail.com>
2022-03-21 21:34:22 -0300

    Binary Heaps extractMax

--- src/DataStructures/BinaryHeaps/index.tsx
+++ src/DataStructures/BinaryHeaps/index.tsx
@@ -24,4 +24,49 @@ export class BinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length: number = this.values.length;
+    const element: any = this.values[0];
+
+    while (true) {
+      let leftChildIdx: number = 2 * idx + 1;
+      let rightChildIdx: number = 2 * idx + 2;
+      let leftChild: any;
+      let rightChild: any;
+      let swap: any = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (rightChild > element) {
+          if ((swap === null && rightChild > element) ||
+            (swap !== null && rightChild > leftChild)) {
+            swap = rightChildIdx;
+          }
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
